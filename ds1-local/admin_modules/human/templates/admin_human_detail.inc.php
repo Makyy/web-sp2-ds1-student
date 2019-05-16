ca37bf8eee8ab6a6cd807fe56002bbe4e18575bc
Kristyna Simova <simova.kr@seznam.cz>
2019-05-16 23:36:26 +0200

    Přidání bodu na obrázek těla po kliknutí (pouze js)

--- ds1-local/admin_modules/human/templates/admin_human_detail.inc.php
+++ ds1-local/admin_modules/human/templates/admin_human_detail.inc.php
@@ -7,14 +7,14 @@
 
     <div class="container-fluid" id="human-defects">
         <div class="row">
-            <div class="col-md-6" style="position: relative">
-                <img id="body-img" src="/admin/template/img/defekty_obyvatel/front.jpg" alt="human body" class="img-fluid">
+            <div id="body-div" class="col-md-6" style="position: relative">
+                <img id="body-img" originwidth="1645" originheight="4010" src="/admin/template/img/defekty_obyvatel/front.jpg" alt="human body" class="img-fluid">
 
                 <?php
                 foreach ($defects as $defect ) { ?>
 
                     <div class="human-label" ratiotop="<?php echo getRatioTop($defect[0]) ?>" ratioleft="<?php echo getRatioLeft($defect[1]) ?>" style="top:<?php echo $defect[0]?>px; left:<?php echo $defect[1]?>px;">
-                        <img src="/admin/template/img/defekty_obyvatel/path-top-right.png" alt="defect" >
+                        <img src="/admin/template/img/defekty_obyvatel/path-top-right.png" alt="defect point" >
                         <label><?php echo $defect[2]?></label>
                     </div>
 
@@ -28,6 +28,7 @@
         <br/>
     </div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="/admin/template/js/defekty_obyvatele/defekty.js"></script>
 
 <?php
--- web/admin/template/js/defekty_obyvatele/defekty.js
+++ web/admin/template/js/defekty_obyvatele/defekty.js
@@ -23,6 +23,35 @@ $(document).ready(function () {
         });
     }
 
+    /*
+     * Adds new point
+     */
+    function addPoint(top, left) {
+
+        var imgHeight = $("#body-img").height();
+        var imgWidth = $("#body-img").width();
+
+        var ratiotop = top / imgHeight;
+        var ratioleft = left / imgWidth;;
+
+        var image = document.createElement("img");
+        image.setAttribute("src", "/admin/template/img/defekty_obyvatel/path-top-right.png");
+        image.setAttribute("alt", "defect point");
+
+        var label = document.createElement("label");
+        label.innerHTML = "DEFECT";
+
+        var pointdiv = document.createElement("div");
+        pointdiv.style.cssText = "top:" + top + "px; left:" + left + "px;";
+        pointdiv.setAttribute("class", "human-label");
+        pointdiv.setAttribute("ratiotop", ratiotop);
+        pointdiv.setAttribute("ratioleft", ratioleft);
+
+        pointdiv.appendChild(image);
+        pointdiv.appendChild(label);
+        document.getElementById("body-div").appendChild(pointdiv);
+    }
+
     movePoints();  // adjust points to actual image size
 
     // define resize event that calls points adjusting to actual image size.
@@ -31,5 +60,20 @@ $(document).ready(function () {
         clearTimeout(timer_id);
         timer_id = setTimeout(movePoints(), 50);
     });
+
+    $("#body-img").on("click", function(e) {
+
+        var offset_t = $(this).offset().top - $(window).scrollTop();
+        var offset_l = $(this).offset().left - $(window).scrollLeft();
+
+        var adjusttop = -18;
+        var adjustleft = 10;
+
+        var left = Math.round( (e.clientX - offset_l) ) + adjustleft;
+        var top = Math.round( (e.clientY - offset_t) ) + adjusttop;
+
+        addPoint(top, left);
+    });
+
     movePoints();
 });
